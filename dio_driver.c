38b971e9c2163f38a555a058c2f7b4d5cd7c979b
haithamgamal <hgh.ramadan89@gmail.com>
2020-11-16 19:17:37 +0200

    created dio_driver.c

---
+++ dio_driver.c
@@ -0,0 +1,12 @@
+/*
+ * dio_driver.c
+ *
+ *  Created on: Nov 16, 2020
+ *      Author: HAITHAM
+ */
+#include<stdio.h>
+#include<stdlib.h>
+#include<avr/io.h>
+#include"dio_driver.h"
+#include<util/delay.h>
+
